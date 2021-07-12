a1e3a722357ab37019eab033c8a2e450c3dc5b38
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-07-11 23:42:01 -0300

    :sparkles: navBar Component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
+import { Link } from "react-scroll";
+import logo from "../assets/logo.png";
 
 function NavBar() {
-  return <div></div>;
+  const [nav, setnav] = useState(false);
+
+  const changeBackgroung = () => {
+    if (window.scrollY >= 50) {
+      setnav(true);
+    } else {
+      setnav(false);
+    }
+  };
+  window.addEventListener("scroll", changeBackgroung);
+  return (
+    <nav className={nav ? "nav active" : "nav"}>
+      <Link to="main" className="logo">
+        <img src={logo} alt="logoSite" />
+      </Link>
+      <input className="menu-btn" type="checkBox" id="menu-btn" />
+      <label className="menu-icon" for="menu-btn">
+        <span className="nav-icon"> </span>
+      </label>
+      <ul className="menu">
+        <li>
+          <Link to="#">Inicio</Link>
+        </li>
+        <li>
+          <Link to="#">Produtos</Link>
+        </li>
+        <li>
+          <Link to="#">Sobre</Link>
+        </li>
+        <li>
+          <Link to="#">Contato</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
